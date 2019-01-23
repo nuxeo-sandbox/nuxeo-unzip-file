bc53695fd84c079142570059d7c6a7537d2b434c
Michaël Vachette <mvachette@nuxeo.com>
2019-01-23 12:36:40 +0900

    Fix and clean up tests

--- nuxeo-unzip-file-core/src/test/java/nuxeo/unzip/file/TestUnzipFileToDocuments.java
+++ nuxeo-unzip-file-core/src/test/java/nuxeo/unzip/file/TestUnzipFileToDocuments.java
@@ -16,7 +16,9 @@
  * Contributors:
  *     Thibaud Arguillere
  *     Michael Vachette
- */package nuxeo.unzip.file;
+ */
+
+package nuxeo.unzip.file;
 
 import org.junit.After;
 import org.junit.Before;
@@ -36,14 +38,13 @@ import org.nuxeo.ecm.core.api.impl.blob.FileBlob;
 import org.nuxeo.ecm.core.test.DefaultRepositoryInit;
 import org.nuxeo.ecm.core.test.annotations.Granularity;
 import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
+import org.nuxeo.ecm.platform.filemanager.api.FileImporterContext;
 import org.nuxeo.ecm.platform.filemanager.api.FileManager;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
-import org.nuxeo.runtime.test.runner.LocalDeploy;
 
 import javax.inject.Inject;
-
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
@@ -54,10 +55,12 @@ import static org.junit.Assert.*;
 @RunWith(FeaturesRunner.class)
 @Features(AutomationFeature.class)
 @RepositoryConfig(init = DefaultRepositoryInit.class, cleanup = Granularity.METHOD)
-@Deploy({ "org.nuxeo.ecm.platform.video.api", "org.nuxeo.ecm.platform.video.core", "org.nuxeo.ecm.platform.picture.api",
-        "org.nuxeo.ecm.platform.picture.core", "org.nuxeo.ecm.platform.filemanager.core",
-        "org.nuxeo.ecm.platform.types.core", "org.nuxeo.ecm.platform.tag", "nuxeo.unzip.file.nuxeo-unzip-file-core" })
-@LocalDeploy({ "nuxeo.unzip.file.nuxeo-unzip-file-core:OSGI-INF/disable-listeners-contrib.xml" })
+@Deploy({
+    "org.nuxeo.ecm.platform.filemanager.core",
+    "org.nuxeo.ecm.platform.types.core",
+    "nuxeo.unzip.file.nuxeo-unzip-file-core",
+    "nuxeo.unzip.file.nuxeo-unzip-file-core:OSGI-INF/disable-listeners-contrib.xml"
+})
 public class TestUnzipFileToDocuments {
 
     public static final String ZIPFILE = "nuxeo-unzip-test.zip";
@@ -87,13 +90,13 @@ public class TestUnzipFileToDocuments {
         assertNotNull(automationService);
         assertNotNull(fileManager);
 
-        PATHS_AND_DOCTYPES = new HashMap<String, String>();
+        PATHS_AND_DOCTYPES = new HashMap<>();
         PATHS_AND_DOCTYPES.put("/nuxeo-unzip-test/File.pdf", "File");
         PATHS_AND_DOCTYPES.put("/nuxeo-unzip-test/f1", "Folder");
         PATHS_AND_DOCTYPES.put("/nuxeo-unzip-test/f1/f1-f1", "Folder");
-        PATHS_AND_DOCTYPES.put("/nuxeo-unzip-test/f1/f1-f1/Video.mp4", "Video");
+        PATHS_AND_DOCTYPES.put("/nuxeo-unzip-test/f1/f1-f1/Video.mp4", "File");
         PATHS_AND_DOCTYPES.put("/nuxeo-unzip-test/f2", "Folder");
-        PATHS_AND_DOCTYPES.put("/nuxeo-unzip-test/f2/Picture.jpg", "Picture");
+        PATHS_AND_DOCTYPES.put("/nuxeo-unzip-test/f2/Picture.jpg", "File");
 
         zipFile = FileUtils.getResourceFileFromContext(ZIPFILE);
         zipFileBlob = new FileBlob(zipFile);
@@ -114,7 +117,6 @@ public class TestUnzipFileToDocuments {
 
     @After
     public void cleanup() {
-
         coreSession.removeDocument(testDocsFolder.getRef());
         coreSession.save();
     }
@@ -128,7 +130,6 @@ public class TestUnzipFileToDocuments {
         for (Entry<String, String> entry : PATHS_AND_DOCTYPES.entrySet()) {
             String subPath = entry.getKey();
             String expectedType = entry.getValue();
-
             testPath = mainParentPath + subPath;
             PathRef pathRef = new PathRef(testPath);
             try {
@@ -141,81 +142,56 @@ public class TestUnzipFileToDocuments {
     }
 
     @Test
-    public void testUnzipToDocuments() throws Exception {
-
+    public void testUnzipToDocuments() {
         DocumentModel mainUnzippedFolderDoc;
-
         mainUnzippedFolderDoc = UnzipToDocuments.run(testDocsFolder, zipFileBlob, null, 0);
         assertNotNull(mainUnzippedFolderDoc);
         assertEquals("nuxeo-unzip-test", mainUnzippedFolderDoc.getTitle());
-
         checkZippedFolderContent();
     }
 
     @Test
     public void testOperation() throws OperationException {
-
-        OperationChain chain;
         OperationContext ctx = new OperationContext(coreSession);
-        assertNotNull(ctx);
-
         ctx.setInput(documentWithZip);
-        chain = new OperationChain("testChain");
+        OperationChain chain = new OperationChain("testChain");
         chain.add(UnzipFileFoDocumentsOp.ID);
         DocumentModel result = (DocumentModel) automationService.run(ctx, chain);
         assertNotNull(result);
-
         checkZippedFolderContent();
-
     }
 
-    @Test
+    @Test(expected = OperationException.class)
     public void testOperationShouldFailWithBlobAndNoTarget() throws OperationException {
-
-        OperationChain chain;
         OperationContext ctx = new OperationContext(coreSession);
-        assertNotNull(ctx);
-
         ctx.setInput(zipFileBlob);
-        chain = new OperationChain("testChain");
+        OperationChain chain = new OperationChain("testChain");
         chain.add(UnzipFileFoDocumentsOp.ID);
-        try {
-            @SuppressWarnings("unused")
-            DocumentModel ignore = (DocumentModel) automationService.run(ctx, chain);
-            assertTrue("Should have thrown an exception", false);
-        } catch (Exception e) {
-            // Automation does not just forward the IllegalArgumentException, it
-            // is embedded in another exception (OPeraitonException, so far)
-            // Can change with evolution of the platform, let's be cool with assertions
-            String cause, message;
-            cause = e.getCause().toString();
-            message = e.getMessage();
-            assertTrue(
-                    message.indexOf("IllegalArgumentException") > -1 || cause.indexOf("IllegalArgumentException") > -1);
-        }
-
+        automationService.run(ctx, chain);
     }
 
     @Test
-    public void tesArchiveCheck() {
-
+    public void testValidArchiveCheck() {
         assertTrue(UnzipFileToDocumentsImporter.looksLikeValidZip(zipFile));
+    }
 
-        assertFalse(UnzipFileToDocumentsImporter.looksLikeValidZip(null));
-
+    @Test
+    public void testInvalidArchiveCheck() {
         File notValidZip = FileUtils.getResourceFileFromContext("not-a-valid-zip.zip");
         assertFalse(UnzipFileToDocumentsImporter.looksLikeValidZip(notValidZip));
     }
 
     @Test
     public void testImportViaFileManager() throws Exception {
+        FileImporterContext context = FileImporterContext.builder(coreSession,
+                zipFileBlob, testDocsFolder.getPathAsString())
+                .overwrite(true)
+                .fileName("TheArchive")
+                .build();
+        DocumentModel doc = fileManager.createOrUpdateDocument(context);
 
-        DocumentModel doc = fileManager.createDocumentFromBlob(coreSession, zipFileBlob,
-                testDocsFolder.getPathAsString(), true, "TheArchive");
-        assertTrue(doc != null);
+        assertNotNull(doc);
         assertEquals("nuxeo-unzip-test", doc.getTitle());
-
         checkZippedFolderContent();
-
     }
 }
